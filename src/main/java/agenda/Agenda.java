5a3cf6108fda7193bf8a541a91f4ba3ded3ad83a
mjule <mjule@Gemini>
2020-12-13 16:17:12 +0100

    Rendu

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -7,6 +7,7 @@ public class Agenda {
 
     public ArrayList<Event> myAgenda = new ArrayList<>();
     public ArrayList<Event> eventsInDay = new ArrayList<>();
+    public ArrayList<Event> sameTitle = new ArrayList<>();
 
     public void addEvent(Event e) {
         myAgenda.add(e);
@@ -25,5 +26,35 @@ public class Agenda {
             }
         }
         return eventsInDay;
+    }
+    
+        /**
+     * Trouver les événements de l'agenda en fonction de leur titre
+     *
+     * @param title le titre à rechercher
+     * @return les événements qui ont le même titre
+     */
+    public List<Event> findByTitle(String title) {
+        for (Event e : myAgenda) {
+            if (e.getTitle().equals(title)) {
+                sameTitle.add(e);
+            }
+        }
+        return sameTitle;
+    }
+    
+    /**
+     * Déterminer s’il y a de la place dans l'agenda pour un événement
+     * @param e L'événement à tester (on se limitera aux événements simples)
+     * @return vrai s’il y a de la place dans l'agenda pour cet événement
+     */
+    public boolean isFreeFor(Event e) {
+        boolean isFree = false;
+        for (Event ev : myAgenda) {
+            if ((ev.getStart().isBefore(e.getStart()) && ev.getEnd().isBefore(e.getStart())) || (ev.getStart().isAfter(e.getStart()))){
+                isFree = true;
+            }
+        }
+        return isFree;
     }
 }
--- src/test/java/agenda/AgendaTest.java
+++ src/test/java/agenda/AgendaTest.java
@@ -4,36 +4,46 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 
 public class AgendaTest {
+
     Agenda agenda;
-    
-    // November 1st, 2020
+    List<Event> testList = new ArrayList<>();
+
+    // On ajoute la date du November 1st, 2020
     LocalDate nov_1_2020 = LocalDate.of(2020, 11, 1);
 
-    // January 5, 2021
+    // On ajoute la date du January 5, 2021
     LocalDate jan_5_2021 = LocalDate.of(2021, 1, 5);
 
-    // November 1st, 2020, 22:30
+    // On ajoute la date du November 1st, 2020, 22:30
     LocalDateTime nov_1__2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
+    
+    // On ajoute la date du May 5th, 2020, 18:30
+    LocalDateTime may_5__2020_18_30 = LocalDateTime.of(2020, 5, 5, 18, 30);
 
-    // 120 minutes
+    // On ajoute la durée de 120 minutes
     Duration min_120 = Duration.ofMinutes(120);
 
     // A simple event
     // November 1st, 2020, 22:30, 120 minutes
     Event simple = new Event("Simple event", nov_1__2020_22_30, min_120);
+    
+    // A second simple event
+    // May 5th, 2020, 18:30, 120 minutes
+    Event simpleBis = new Event("Simple event bis", may_5__2020_18_30, min_120);
 
     // A Weekly Repetitive event ending at a given date
     RepetitiveEvent fixedTermination = new FixedTerminationEvent("Fixed termination weekly", nov_1__2020_22_30, min_120, ChronoUnit.WEEKS, jan_5_2021);
 
     // A Weekly Repetitive event ending after a give number of occurrrences
     RepetitiveEvent fixedRepetitions = new FixedTerminationEvent("Fixed termination weekly", nov_1__2020_22_30, min_120, ChronoUnit.WEEKS, 10);
-    
+
     // A daily repetitive event, never ending
     // November 1st, 2020, 22:30, 120 minutes
     RepetitiveEvent neverEnding = new RepetitiveEvent("Never Ending", nov_1__2020_22_30, min_120, ChronoUnit.DAYS);
@@ -42,16 +52,27 @@ public class AgendaTest {
     public void setUp() {
         agenda = new Agenda();
         agenda.addEvent(simple);
+        agenda.addEvent(simpleBis);
         agenda.addEvent(fixedTermination);
         agenda.addEvent(fixedRepetitions);
         agenda.addEvent(neverEnding);
     }
-    
+
     @Test
     public void testMultipleEventsInDay() {
         assertEquals(4, agenda.eventsInDay(nov_1_2020).size(), "Il y a 4 événements ce jour là");
         assertTrue(agenda.eventsInDay(nov_1_2020).contains(neverEnding));
     }
 
+    @Test
+    public void testFindByTitle() {
+        testList.add(simple);
+        assertEquals(testList, agenda.findByTitle("Simple event"), "Les évènements ne sont pas correctement triés selon leur titre");
+    }
+    
+    @Test
+    public void testIsFreeFor(){
+        assertTrue(agenda.isFreeFor(simpleBis),"L'évènement devrait être ajouté à l'agenda");
+    }
 
 }
